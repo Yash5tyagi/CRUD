5fdc7ed0b54e513fdf1147bee6d8348895f7ad38
Yash5tyagi <yash.tyagi@pantelwar.com>
2023-11-09 10:40:29 +0530

    added on conflict

--- common/db/create.go
+++ common/db/create.go
@@ -3,21 +3,25 @@ package db
 import (
 	"CRUD/common/views"
 	"context"
+	"fmt"
 	"log"
 )
 
 func (db *DB) InsertStudent(stud *views.Student) {
-	querystring := "INSERT INTO students (first_name,last_name,roll_no) VALUES ($1,$2,$3) ON CONFLICT(roll_no) RETURNING id"
+	querystring := "INSERT INTO students (first_name,last_name,roll_no) VALUES ($1,$2,$3) ON CONFLICT(roll_no) DO NOTHING RETURNING id"
 	err := db.Conn.QueryRow(context.Background(), querystring, stud.FirstName, stud.LastName, stud.RollNo).Scan(&stud.Id)
 	if err != nil {
+		fmt.Printf("unable to insert student \n")
 		log.Fatal(err)
 	}
 }
 
 func (db *DB) InsertParent(parent *views.Parents) {
-	querystring := "INSERT INTO parents (father_name,mother_name,sid) VALUES ($1,$2,$3) "
+	querystring := "INSERT INTO parents (father_name,mother_name,sid) VALUES ($1,$2,$3) ON CONFLICT(sid) DO NOTHING"
 	_, err := db.Conn.Query(context.Background(), querystring, parent.FatherName, parent.MotherName, parent.SId)
 	if err != nil {
+		fmt.Printf("unable to insert parents\n")
 		log.Fatal(err)
+
 	}
 }
--- common/handler/readHandler.go
+++ common/handler/readHandler.go
@@ -58,3 +58,44 @@ func GetALLStudent(conn db.DB) gin.HandlerFunc {
 	}
 
 }
+
+func GetAll(conn db.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		rows := conn.SelectParent()
+
+		defer rows.Close()
+		var parents []views.Parents
+		for rows.Next() {
+			var parent views.Parents
+
+			if err := rows.Scan(&parent.FatherName, &parent.MotherName); err != nil {
+
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+			parents = append(parents, parent)
+		}
+
+		rows = conn.SelectStudent()
+
+		defer rows.Close()
+		var students []views.Student
+		for rows.Next() {
+			var student views.Student
+
+			if err := rows.Scan(&student.FirstName, &student.LastName, &student.RollNo); err != nil {
+
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			}
+			students = append(students, student)
+		}
+		fmt.Println("Students:")
+		for _, student := range students {
+			fmt.Printf("first name: %s, last name: %s, roll no.: %d\n", student.FirstName, student.LastName, student.RollNo)
+		}
+		fmt.Println("Parents:")
+		for _, parent := range parents {
+			fmt.Printf("father name: %s, mother name: %s\n", parent.FatherName, parent.MotherName)
+		}
+		c.JSON(http.StatusOK, parents)
+	}
+}
