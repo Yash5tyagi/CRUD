a181193e318dd16715ee1b217781ced0c57c863c
Yash5tyagi <yash.tyagi@pantelwar.com>
2023-11-09 11:10:58 +0530

    added on conflict update

--- common/db/create.go
+++ common/db/create.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (db *DB) InsertStudent(stud *views.Student) {
-	querystring := "INSERT INTO students (first_name,last_name,roll_no) VALUES ($1,$2,$3) ON CONFLICT(roll_no) DO NOTHING RETURNING id"
+	querystring := "INSERT INTO students (first_name,last_name,roll_no) VALUES ($1,$2,$3) ON CONFLICT(roll_no) DO UPDATE SET first_name=$1,last_name=$2 RETURNING id"
 	err := db.Conn.QueryRow(context.Background(), querystring, stud.FirstName, stud.LastName, stud.RollNo).Scan(&stud.Id)
 	if err != nil {
 		fmt.Printf("unable to insert student \n")
@@ -17,7 +17,7 @@ func (db *DB) InsertStudent(stud *views.Student) {
 }
 
 func (db *DB) InsertParent(parent *views.Parents) {
-	querystring := "INSERT INTO parents (father_name,mother_name,sid) VALUES ($1,$2,$3) ON CONFLICT(sid) DO NOTHING"
+	querystring := "INSERT INTO parents (father_name,mother_name,sid) VALUES ($1,$2,$3) ON CONFLICT(sid) DO UPDATE SET father_name=$1,mother_name=$2"
 	_, err := db.Conn.Query(context.Background(), querystring, parent.FatherName, parent.MotherName, parent.SId)
 	if err != nil {
 		fmt.Printf("unable to insert parents\n")
